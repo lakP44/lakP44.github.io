ace4e56f5f91bb4d280bb2d9637d98a6c913efec
KRH <110088655+lakP44@users.noreply.github.com>
2023-11-19 18:54:09 +0900

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// JavaScript 코드 작성
-document.addEventListener('DOMContentLoaded', function () {
-    // DOM이 로드된 후 실행될 JavaScript 코드
-    console.log('JavaScript 파일이 로드되었습니다.');
-    // 여기에 추가적인 JavaScript 코드를 작성할 수 있습니다.
-});
